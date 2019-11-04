7ca1835bff44a6925b0cbbcf5e34a7676118cd43
Stefan Majer <stefan.majer@f-i-ts.de>
2019-11-04 10:39:36 +0100

    Add convenience function for tag identification

--- tag.go
+++ tag.go
@@ -2,10 +2,23 @@ package metalgo
 
 import (
 	"fmt"
+	"strings"
 )
 
-// TagServicePrefix the prefix of the tag used to identify services
-const TagServicePrefix = "cluster.metal-pod.io/clusterid/namespace/servicename"
+const (
+	// TagClusterQualifier identifies the cluster
+	TagClusterQualifier = "clusterid"
+	// TagServiceQualifier identifies the service
+	TagServiceQualifier = "service"
+	// TagNamespaceQualifier identifies the namespace
+	TagNamespaceQualifier = "namespace"
+
+	// TagClusterPrefix the prefix of the tag used to identify cluster
+	TagClusterPrefix = "cluster.metal-pod.io/" + TagClusterQualifier
+
+	// TagServicePrefix the prefix of the tag used to identify services
+	TagServicePrefix = TagClusterPrefix + "/" + TagNamespaceQualifier + "/" + TagServiceQualifier
+)
 
 // BuildServiceTag constructs the service tag for the given cluster and service
 func BuildServiceTag(clusterID string, namespace, serviceName string) string {
@@ -15,4 +28,18 @@ func BuildServiceTag(clusterID string, namespace, serviceName string) string {
 // BuildServiceTagClusterPrefix constructs the prefix of the service tag that identify all services of a cluster
 func BuildServiceTagClusterPrefix(clusterID string) string {
 	return fmt.Sprintf("%s=%s", TagServicePrefix, clusterID)
+}
+
+// TagIsMemberOfCluster returns true of the given tag is a clustertag and clusterID matches.
+func TagIsMemberOfCluster(tag, clusterID string) bool {
+	if strings.HasPrefix(tag, TagClusterPrefix) {
+		parts := strings.Split(tag, "=")
+		if len(parts) != 2 {
+			return false
+		}
+		if strings.HasPrefix(parts[1], clusterID) {
+			return true
+		}
+	}
+	return false
 }
