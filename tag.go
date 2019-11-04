89e23522d368a6a41f09016c8ac6b901b99c3f8e
Markus Fensterer <markus.fensterer@gmail.com>
2019-11-04 10:05:01 +0100

    BuildServiceTag fix

--- tag.go
+++ tag.go
@@ -9,7 +9,7 @@ const TagServicePrefix = "cluster.metal-pod.io/clusterid/namespace/servicename"
 
 // BuildServiceTag constructs the service tag for the given cluster and service
 func BuildServiceTag(clusterID string, namespace, serviceName string) string {
-	return fmt.Sprintf("%s/%s%s", BuildServiceTagClusterPrefix(clusterID), namespace, serviceName)
+	return fmt.Sprintf("%s/%s/%s", BuildServiceTagClusterPrefix(clusterID), namespace, serviceName)
 }
 
 // BuildServiceTagClusterPrefix constructs the prefix of the service tag that identify all services of a cluster
