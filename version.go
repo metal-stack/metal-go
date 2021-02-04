667fff6022ca4f46337a5b258595c20bae02061f
Gerrit <tirreg91@googlemail.com>
2021-02-04 09:23:27 +0100

    Add wrappers for health and version.

---
+++ version.go
@@ -0,0 +1,21 @@
+package metalgo
+
+import (
+	"github.com/metal-stack/metal-go/api/models"
+)
+
+// VersionGetResponse is the response of a VersionGet action
+type VersionGetResponse struct {
+	Version *models.RestVersion
+}
+
+// VersionGet return a Version
+func (d *Driver) VersionGet() (*VersionGetResponse, error) {
+	response := &VersionGetResponse{}
+	resp, err := d.client.Version.Info(nil, nil)
+	if err != nil {
+		return response, err
+	}
+	response.Version = resp.Payload
+	return response, nil
+}
