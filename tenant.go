30ca56a97baaf9f068f30900ee48b60c92de9de3
abloss <8138505+droid42@users.noreply.github.com>
2021-03-23 16:14:09 +0100

    Get and list tenants (#59)

---
+++ tenant.go
@@ -0,0 +1,48 @@
+package metalgo
+
+import (
+	"github.com/metal-stack/metal-go/api/client/tenant"
+	"github.com/metal-stack/metal-go/api/models"
+)
+
+// TenantListResponse is the response of a TenantList action
+type TenantListResponse struct {
+	Tenant []*models.V1TenantResponse
+}
+
+// TenantGetResponse is the response of a TenantGet action
+type TenantGetResponse struct {
+	Tenant *models.V1TenantResponse
+}
+
+// TenantFindRequest is the find request struct
+type TenantFindRequest struct {
+	ID     string
+	Name   string
+	Tenant string
+}
+
+// TenantList return all Tenants
+func (d *Driver) TenantList() (*TenantListResponse, error) {
+	response := &TenantListResponse{}
+	listTenants := tenant.NewListTenantsParams()
+	resp, err := d.tenant.ListTenants(listTenants, nil)
+	if err != nil {
+		return response, err
+	}
+	response.Tenant = resp.Payload
+	return response, nil
+}
+
+// TenantGet return a Tenant
+func (d *Driver) TenantGet(TenantID string) (*TenantGetResponse, error) {
+	response := &TenantGetResponse{}
+	getTenant := tenant.NewGetTenantParams()
+	getTenant.ID = TenantID
+	resp, err := d.tenant.GetTenant(getTenant, nil)
+	if err != nil {
+		return response, err
+	}
+	response.Tenant = resp.Payload
+	return response, nil
+}
