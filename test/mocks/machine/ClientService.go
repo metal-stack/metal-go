93e481c27e3c5bb4ba2986b56a79540266c70374
Stefan Majer <stefan.majer@f-i-ts.de>
2022-03-25 10:45:30 +0100

    Machine Events

--- test/mocks/machine/ClientService.go
+++ test/mocks/machine/ClientService.go
@@ -74,6 +74,36 @@ func (_m *ClientService) AddProvisioningEvent(params *machine.AddProvisioningEve
 	return r0, r1
 }
 
+// AddProvisioningEvents provides a mock function with given fields: params, authInfo, opts
+func (_m *ClientService) AddProvisioningEvents(params *machine.AddProvisioningEventsParams, authInfo runtime.ClientAuthInfoWriter, opts ...machine.ClientOption) (*machine.AddProvisioningEventsOK, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, params, authInfo)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *machine.AddProvisioningEventsOK
+	if rf, ok := ret.Get(0).(func(*machine.AddProvisioningEventsParams, runtime.ClientAuthInfoWriter, ...machine.ClientOption) *machine.AddProvisioningEventsOK); ok {
+		r0 = rf(params, authInfo, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*machine.AddProvisioningEventsOK)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*machine.AddProvisioningEventsParams, runtime.ClientAuthInfoWriter, ...machine.ClientOption) error); ok {
+		r1 = rf(params, authInfo, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // AllocateMachine provides a mock function with given fields: params, authInfo, opts
 func (_m *ClientService) AllocateMachine(params *machine.AllocateMachineParams, authInfo runtime.ClientAuthInfoWriter, opts ...machine.ClientOption) (*machine.AllocateMachineOK, error) {
 	_va := make([]interface{}, len(opts))
