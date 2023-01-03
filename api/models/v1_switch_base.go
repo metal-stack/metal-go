7624fecb162c0da36b22572eab25c92e05f240c7
Stefan Majer <stefan.majer@f-i-ts.de>
2023-01-03 11:51:33 +0100

    Support more switch details

--- api/models/v1_switch_base.go
+++ api/models/v1_switch_base.go
@@ -19,8 +19,20 @@ import (
 // swagger:model v1.SwitchBase
 type V1SwitchBase struct {
 
+	// command to access the console of the switch
+	ConsoleCommand string `json:"console_command,omitempty" yaml:"console_command,omitempty"`
+
+	// the ip address of the management interface of the switch
+	ManagementIP string `json:"management_ip,omitempty" yaml:"management_ip,omitempty"`
+
+	// the user to connect to the switch
+	ManagementUser string `json:"management_user,omitempty" yaml:"management_user,omitempty"`
+
 	// the mode the switch currently has
 	Mode string `json:"mode,omitempty" yaml:"mode,omitempty"`
+
+	// the operating system the switch currently has
+	Os *V1SwitchOS `json:"os,omitempty" yaml:"os,omitempty"`
 
 	// the id of the rack in which this switch is located
 	// Required: true
@@ -31,6 +43,10 @@ type V1SwitchBase struct {
 func (m *V1SwitchBase) Validate(formats strfmt.Registry) error {
 	var res []error
 
+	if err := m.validateOs(formats); err != nil {
+		res = append(res, err)
+	}
+
 	if err := m.validateRackID(formats); err != nil {
 		res = append(res, err)
 	}
@@ -38,6 +54,25 @@ func (m *V1SwitchBase) Validate(formats strfmt.Registry) error {
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
+	return nil
+}
+
+func (m *V1SwitchBase) validateOs(formats strfmt.Registry) error {
+	if swag.IsZero(m.Os) { // not required
+		return nil
+	}
+
+	if m.Os != nil {
+		if err := m.Os.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("os")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("os")
+			}
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -50,8 +85,33 @@ func (m *V1SwitchBase) validateRackID(formats strfmt.Registry) error {
 	return nil
 }
 
-// ContextValidate validates this v1 switch base based on context it is used
+// ContextValidate validate this v1 switch base based on the context it is used
 func (m *V1SwitchBase) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.contextValidateOs(ctx, formats); err != nil {
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+func (m *V1SwitchBase) contextValidateOs(ctx context.Context, formats strfmt.Registry) error {
+
+	if m.Os != nil {
+		if err := m.Os.ContextValidate(ctx, formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("os")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("os")
+			}
+			return err
+		}
+	}
+
 	return nil
 }
 
