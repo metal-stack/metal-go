64a195204d139ee6df759a899cc0c9538581d1df
auto-build <info@metal-stack.io>
2024-02-15 10:09:05 +0000

    Bump to version v0.28.0

--- test/mocks/ip/ClientService.go
+++ test/mocks/ip/ClientService.go
@@ -199,43 +199,6 @@ func (_m *ClientService) FreeIP(params *clientip.FreeIPParams, authInfo runtime.
 	return r0, r1
 }
 
-// FreeIPDeprecated provides a mock function with given fields: params, authInfo, opts
-func (_m *ClientService) FreeIPDeprecated(params *clientip.FreeIPDeprecatedParams, authInfo runtime.ClientAuthInfoWriter, opts ...clientip.ClientOption) (*clientip.FreeIPDeprecatedOK, error) {
-	_va := make([]interface{}, len(opts))
-	for _i := range opts {
-		_va[_i] = opts[_i]
-	}
-	var _ca []interface{}
-	_ca = append(_ca, params, authInfo)
-	_ca = append(_ca, _va...)
-	ret := _m.Called(_ca...)
-
-	if len(ret) == 0 {
-		panic("no return value specified for FreeIPDeprecated")
-	}
-
-	var r0 *clientip.FreeIPDeprecatedOK
-	var r1 error
-	if rf, ok := ret.Get(0).(func(*clientip.FreeIPDeprecatedParams, runtime.ClientAuthInfoWriter, ...clientip.ClientOption) (*clientip.FreeIPDeprecatedOK, error)); ok {
-		return rf(params, authInfo, opts...)
-	}
-	if rf, ok := ret.Get(0).(func(*clientip.FreeIPDeprecatedParams, runtime.ClientAuthInfoWriter, ...clientip.ClientOption) *clientip.FreeIPDeprecatedOK); ok {
-		r0 = rf(params, authInfo, opts...)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*clientip.FreeIPDeprecatedOK)
-		}
-	}
-
-	if rf, ok := ret.Get(1).(func(*clientip.FreeIPDeprecatedParams, runtime.ClientAuthInfoWriter, ...clientip.ClientOption) error); ok {
-		r1 = rf(params, authInfo, opts...)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // ListIPs provides a mock function with given fields: params, authInfo, opts
 func (_m *ClientService) ListIPs(params *clientip.ListIPsParams, authInfo runtime.ClientAuthInfoWriter, opts ...clientip.ClientOption) (*clientip.ListIPsOK, error) {
 	_va := make([]interface{}, len(opts))
